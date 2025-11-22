499c73beb117d8ca2ab438876d0ec48324490d3b
celine-doc <celinart.arkam@gmail.com>
2025-11-22 17:22:35 +0100

    Update login.php

--- include/login.php
+++ include/login.php
@@ -9,12 +9,16 @@ function loginUser(string $identifiant, string $password): array
             WHERE code_utilisateur::text = $1";
     
     $result = pg_query_params($db, $sql, [$identifiant]);
-    if (!$result) {
-        return ['success' => false, 'error' => 'Erreur requête'];
+
+    // Si la requête échoue ou aucun utilisateur trouvé
+    if (!$result || pg_num_rows($result) === 0) {
+        return ['success' => false, 'error' => 'Identifiant ou mot de passe incorrect'];
     }
 
     $user = pg_fetch_assoc($result);
-    if (!$user || $password !== $user['mot_de_passe_hache']) {
+
+    // on utilise password_verify() qui compare le mot de passe saisi en clair au mdp haché de la base 
+    if (!password_verify($password, $user['mot_de_passe_hache'])) {
         return ['success' => false, 'error' => 'Identifiant ou mot de passe incorrect'];
     }
 
