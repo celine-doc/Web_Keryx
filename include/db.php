6c0cad5c7e59b19a99aae369868d168e2ab96ef9
celine-doc <celinart.arkam@gmail.com>
2025-11-22 11:45:47 +0100

    Update db.php

--- include/db.php
+++ include/db.php
@@ -1,1 +1,28 @@
+<?php
+
+// communique avec la VM BDD 
+
+function getDb() {
+    static $db = null;
+
+    if ($db === null) {
+        $host = '192.168.168.101';   // IP de la VM BDD
+        $port = '5433'; // port d'ecoute 
+        $dbname = 'postgres';
+        $user = 'webuser';
+        $password = 'keryx';
+
+        $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+        if (!$conn) {
+            die(json_encode(["success" => false, "error" => pg_last_error()]));
+        }
+
+        $db = $conn;
+    }
+
+    return $db;
+}
+?>
+
 
