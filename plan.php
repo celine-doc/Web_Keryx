7e280c994687d051c83d3de3b4f82b6605501cc8
celine-doc <celinart.arkam@gmail.com>
2025-11-29 17:26:41 +0100

    Add files via upload

---
+++ plan.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="./css/styles.css"/>
+    <link rel="shortcut icon" type="image/jpg" href="./images/favicon.jpg"/>
+    <title>plan du site - Kéryx</title>
+    
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="compte.php">Mon compte</a></li>
+            </ul>
+        </nav>
+    </header>
+
+     <main>
+    <h1>Plan du site</h1>
+
+    <section class="plan-lien" style="color:#cfd8ff;" >
+        <h2>Sommaire</h2>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="compte.php">Mon compte</a></li>
+            <li><a href="afficheurs.php">Rechercher les afficheurs par tronçon</a></li>
+            <li><a href="message.php">Ajouter un message</a></li>
+            <li><a href="campagne.php">Voir les Campagnes en cours</a></li>
+            <li><a href="a_propos.php"> à propos du site </a></li>
+        </ul>
+    </section>
+</main>
+
+
+    <footer>
+        <div class="footer-container">
+            <div class="footer-section">
+                <h4>Informations</h4>
+                <p>Réalisé par Céline ARKAM - Benjamin Zivic - Tsantan'ny avo Razoliferason</p>
+                <a href="plan.php">Plan du site</a>
+            </div>
+            
+            <div class="footer-section">
+                <h4>Organisme</h4>
+                <p>CY Cergy Paris Université 2025</p>
+                <p>Mis à jour le : <strong>30/11/2025</strong></p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
